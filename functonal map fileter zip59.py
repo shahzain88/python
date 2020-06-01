81f2291a8dd2adef5c1d8cc5b77bee0416cf49ad
shahzain88 <59013209+shahzain88@users.noreply.github.com>
2020-06-02 07:20:20 +0900

    map(),filter(),zip() and the mother of them is a reduce()

--- functonal map fileter zip59.py
+++ functonal map fileter zip59.py
@@ -1,8 +1,7 @@
+from functools import reduce
 my_list = [1, 2, 3]
 # even if this is tupple it will give you answer
-your_list = {
-    [(10), (20), (30)]
-}
+your_list = [10, 20, 30]
 
 
 def multiply_by2(item):
@@ -10,9 +9,20 @@ def multiply_by2(item):
 
 
 def only_odd(item):
+    # Becouse of '!='  python comepares right side and left side  if item % 2 id not = to 0 python will return Ture.
     return item % 2 != 0
 
 
+def accumulator(acc, item):
+    print(acc, item)
+    return acc + item
+
+
+# map,filter,zip DOSE NOT modify the original data (list) ,BUT it makes NEW DATA (list) ,
+# list will turn the result of that location to a list shape .
 print(list(map(multiply_by2, my_list)))
+# Filter workes with boolian (True and False) , if (onlly_odd,my_list ) True put it in the same location  as a result, if not then pop it out from thr location .
 print(list(filter(only_odd, my_list)))
 print(list(zip(my_list, your_list)))
+# we dont need to chang the data to list .
+print(reduce(accumulator, my_list, 0))
