69d5d571f7638194407ef978eeeda985e00fcf9b
shahzain88 <59013209+shahzain88@users.noreply.github.com>
2020-06-02 00:10:54 +0900

    map , filter ,zip ,

--- functonal map fileter zip59.py
+++ functonal map fileter zip59.py
@@ -1,4 +1,5 @@
 my_list = [1, 2, 3]
+your_list = [10, 20, 30]
 
 
 def multiply_by2(item):
@@ -11,3 +12,4 @@ def only_odd(item):
 
 print(list(map(multiply_by2, my_list)))
 print(list(filter(only_odd, my_list)))
+print(list(zip(my_list, your_list)))
